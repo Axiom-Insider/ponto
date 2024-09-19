c9fc7ef2a9a630fbdb2bf788ccea93c52110cf3d
Axiom-Insider <franciscomartins0006@gmail.com>
2024-09-19 15:04:31 -0300

    colocando apra ser direcionado a rota login

--- frontend/server.js
+++ frontend/server.js
@@ -47,7 +47,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get('/', (req, res)=>[
-  res.send('teste')
+  res.redirect('/login')
 ])
 
 //rotas 
